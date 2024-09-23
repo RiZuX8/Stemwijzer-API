83cc6caa1f01479f894146c3bfa68d05ca653f9f
OscarZoomer <oszoomer@gmail.com>
2024-09-23 15:19:57 +0200

    Update Statement.php

--- src/Statement.php
+++ src/Statement.php
@@ -2,5 +2,18 @@
 
 class Statement
 {
+    public $statementID;
+    public $name;
+    public $description;
+    public $image;
+    public $xValue;
+    public $yValue;
 
+    private $conn;
+    private $table = "statements";
+
+    public function __construct(PDO $db)
+    {
+        $this->conn = $db;
+    }
 }
