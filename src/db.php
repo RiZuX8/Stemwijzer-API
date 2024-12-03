2dff5f1bf1c8d239a48ab93c45da65f65c80594b
ZoomerDev <oszoomer@gmail.com>
2024-12-03 11:33:32 +0100

    Update db.php

--- src/db.php
+++ src/db.php
@@ -20,6 +20,7 @@ class db
                 $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->db_name;
                 $mycon = new PDO($dsn, $this->username, $this->password);
                 $mycon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $mycon->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             } catch (PDOException $exception) {
                 error_log("Connection error: " . $exception->getMessage());
                 echo "Connection error: " . $exception->getMessage();
